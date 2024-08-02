c55ce761b4b13c971ff7f30e20b14188440f34a1
alijafarkamal <ali.jafar.fast@gmail.com>
2024-08-02 11:33:46 +0500

    Initial commit

---
+++ third.r
@@ -0,0 +1,12 @@
+
+music_genre <- factor(c("Jazz", "Rock", "Classic", "Classic", "Pop", "Jazz", "Rock", "Jazz"))
+
+music_genre
+
+
+games_genre <- factor(c("kids","adults","ladies"))
+games_genre
+
+music_genre <- factor(c("Jazz", "Rock", "Classic", "Classic", "Pop", "Jazz", "Rock", "Jazz"))
+
+levels(music_genre)
